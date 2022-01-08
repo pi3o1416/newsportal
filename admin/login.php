e29e149aadcb00b4a2adf338714d769271cd9a79
monir hossain <mmonir.m.29@gmail.com>
2022-01-08 17:05:11 +0600

    initial-commit

---
+++ admin/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include '../base.php'; ?>
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="is-size-2 has-text-primary">Admin Login</h1>
+        <div class="box column is-4 is-offset-4" style="margin-top:12%">
+            <form action="action.php" method="post">
+                <label class="label" for="Username">Username</label>
+                <input class="input" type="text" name="username" id="username" value=""><br>
+                <label class="label" for="Password">Password</label>
+                <input class="input" type="password" name="password" id="password" value="">
+                <button class="button is-primary" type="submit" style="margin-top:5%">Submit</button><br>
+                <a href="#">Forget Password?</a>
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ base.php
@@ -0,0 +1,4 @@
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width" />
+<link rel="stylesheet" href="http://localhost/newsportal/static/css/main.css" type="text/css" charset="utf-8">
+
