54b89b2014d9111c232f4198b390cdf4c6170d4a
monir hossain <mmonir.m.29@gmail.com>
2022-01-13 20:23:33 +0600

    add regexp email validation

--- static/js/validation.js
+++ static/js/validation.js
@@ -7,32 +7,45 @@ function validate_username() {
         data: {username: username},
         success: function (data) {
             var message = document.getElementById('message')
+            var button = document.getElementById('user_create_form_button')
             if (data === '\nsuccess') {
                 message.innerHTML = '<h1>Username already exist</h1>'
                 message.className = 'has-text-danger'
+                button.disabled = true
             } else {
                 message.innerHTML = ''
+                button.disabled = false
             }
         }
     })
 }
 
 function validate_email() {
+    var button = document.getElementById('user_create_form_button')
     var email = document.forms['user_create_form']['email'].value
-    $.ajax({
-        url: 'http://localhost/newsportal/admin/components/check_email.php',
-        type: 'post',
-        data: {email: email},
-        success: function (data) {
-            var message = document.getElementById('message')
-            if (data === '\nsuccess') {
-                message.innerHTML = '<h1>Email already exist</h1>'
-                message.className = 'has-text-danger'
-            } else {
-                message.innerHTML = ''
+    var regular_expression = /\S+@\S+\.\S+/;
+    if (!regular_expression.test(email)) {
+        message.innerHTML = '<h1>Invalid Email</h1>'
+        message.className = 'has-text-danger'
+        button.disabled = true
+    } else {
+        $.ajax({
+            url: 'http://localhost/newsportal/admin/components/check_email.php',
+            type: 'post',
+            data: {email: email},
+            success: function (data) {
+                var message = document.getElementById('message')
+                if (data === '\nsuccess') {
+                    message.innerHTML = '<h1>Email already exist</h1>'
+                    message.className = 'has-text-danger'
+                    button.disabled = true
+                } else {
+                    message.innerHTML = ''
+                    button.disabled = false
+                }
             }
-        }
-    })
+        })
+    }
 }
 
 function validate_first_name() {
